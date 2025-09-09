2c16eb2c7af137e2bdf526284bf98ebf988f8fd0
YourName <youremail@example.com>
2025-09-10 00:15:51 +0700

    Config

--- config/admin.js
+++ config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'default-admin-jwt-secretqKINuec2IOMRktvEKY4+Fb7o93EUlKDXqZDtOa+dTjc='),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'default-api-token-salth9tRa6+90EcyEnkL79qB2er5deskbGqxvgH8RSm6ld8='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'default-transfer-token-saltwEs9nX826J34jEPpudA9td7Chel06Jg0AOal3l1FKJ4='),
     },
   },
   flags: {
